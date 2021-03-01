9a5c6e5a36f22d4042a17e3203afe98abdac7d1e
André Michelli <andremichelli0108@gmail.com>
2021-02-28 22:03:03 -0300

    criada classe para posicionamento da peça no tabuleiro

---
+++ Xadrez/Jogo de xadrez/PosicaXadrez.cs
@@ -0,0 +1,24 @@
+﻿
+using tabuleiro;
+
+namespace Xadrez{
+    class PosicaXadrez {
+
+        public char coluna { get; set; }
+        public int linha { get; set; }
+        
+        public PosicaXadrez(char coluna, int linha) {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public posicao toPosicao() {
+            return new posicao(8 - linha, coluna - 'a');
+        }
+
+        public override string ToString() {
+            return "" + coluna + linha;
+        }
+
+    }
+}
